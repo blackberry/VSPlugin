c28b5c21f1780afd815d4fb42566c403769b2fa2
David Burgess <dburgess@rim.com>
2013-05-03 12:15:00 -0400

    Created unit test for WriteDebuggerArgs.cs

---
+++ src/VSNDK.Tasks.Test/WriteDebuggerArgsTestClass.cs
@@ -0,0 +1,81 @@
+﻿//* Copyright 2010-2011 Research In Motion Limited.
+//*
+//* Licensed under the Apache License, Version 2.0 (the "License");
+//* you may not use this file except in compliance with the License.
+//* You may obtain a copy of the License at
+//*
+//* http://www.apache.org/licenses/LICENSE-2.0
+//*
+//* Unless required by applicable law or agreed to in writing, software
+//* distributed under the License is distributed on an "AS IS" BASIS,
+//* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//* See the License for the specific language governing permissions and
+//* limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using VSNDK.Tasks;
+using System.IO;
+
+namespace VSNDK.Tasks.Test
+{
+    [TestFixture]
+    class WriteDebuggerArgsTestClass
+    {
+        private WriteDebuggerArgs target;
+
+        [SetUp]
+        public void setup()
+        {
+            target = new WriteDebuggerArgs();
+            target.AppName = "HelloWorld";
+            target.Device = "169.254.0.1";
+            target.isSimulator = false;
+            target.OutDir = @"\Device-Debug";
+            target.Password = "AQAAANCMnd8BFdERjHoAwE/Cl+sBAAAA7fSRRS7m1Em+yoK0ioAYhwQAAAACAAAAAAADZgAAwAAAABAAAABtBZa+ZRR0uoriYLDYxqPlAAAAAASAAACgAAAAEAAAAGJAoUM+qvmrXoLeokZQinMQAAAAS1JPN9qlZFQbSZmgaa509xQAAADKl++zEjFNnrI1aKXGrvvmgLRkeQ==";
+            target.ProjectDir = @"C:\Users\dburgess\Desktop\HelloWorld\HelloWorld";
+            target.PublicKeyPath = @"C:\Users\dburgess\AppData\Local/Research In Motion/bbt_id_rsa.pub";
+            target.ToolsPath = @"C:\bbndk-2.0.0\host\win32\x86usr\bin";
+        }
+
+        /// <summary>
+        /// Constructor Test
+        /// </summary>
+        [TestCase]
+        public void WriteDebuggerArgsConstructorTest()
+        {
+            Assert.IsNotNull(target);
+        }
+
+        /// <summary>
+        /// Perform Task Execute test case
+        /// </summary>
+        [TestCase]
+        public void ExecuteTest()
+        {
+            /// Prepare test case
+            bool result = target.Execute();
+
+            /// Verify test case
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// Perform Task Execute test case
+        /// </summary>
+        [TestCase]
+        public void ExecuteFileExistsTest()
+        {
+            /// Prepare test case
+            WriteDebuggerArgs target = new WriteDebuggerArgs();
+            bool targetResult = target.Execute();
+            bool result = File.Exists(target.AppData);
+
+            /// Verify test case
+            Assert. IsTrue(result);
+        }
+    }
+}
