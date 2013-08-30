a283d1382ddf72028430605ee15fdbecc528b594
David Burgess <dburgess@rim.com>
2013-08-29 22:30:13 -0400

    Fived build script to hopefully handle new build on vs2012

--- VSNDKPluginBuild.bat
+++ VSNDKPluginBuild.bat
@@ -7,7 +7,11 @@ set thisDir=%~dp0
 set buildresults="%thisDir%\..\buildresults"
 set dotNetFrameworkPath="C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MsBuild.exe"
 set solutionPath2010="%thisDir%\src\VSNDK.sln"
-set solutionPath2012="%thisDir%\src_vs2012\VSNDK.sln"
+set AddIn2012="%thisDir%\src_vs2012\VSNDK.AddIn\VSNDK.AddIn.csproj"
+set DebugEngine2012="%thisDir%\src_vs2012\VSNDK.DebugEngine\VSNDK.DebugEngine.csproj"
+set GDWrapper2012="%thisDir%\src_vs2012\GDBWrapper\GDBWrapper.vcxproj"
+set VSPackage2012="%thisDir%\src_vs2012\VSNDK.Package\VSNDK.Package.csproj"
+set VSTasks2012="%thisDir%\src_vs2012\VSNDK.Tasks\VSNDK.Tasks.csproj"
 
 REM ********************************************************************************************
 REM Clean up old bin folder
@@ -29,7 +33,10 @@ REM ****************************************************************************
 REM Build VS2010
 REM ********************************************************************************************
 echo Building Solution for VS2012
-%dotNetFrameworkPath% %solutionPath2012% /property:Configuration=Release /target:Clean;Build /p:OutputPath="%buildresults%\VS2012" > %buildresult%\VS2012_buildlog.txt
+%dotNetFrameworkPath% %AddIn2012% /property:Configuration=Release /target:Clean;Build /p:VisualStudioVersion=11.0 /p:OutputPath="%buildresults%\VS2012" > %buildresult%\VS2012_buildlog.txt
+%dotNetFrameworkPath% %DebugEngine2012% /property:Configuration=Release /target:Clean;Build /p:VisualStudioVersion=11.0 /p:OutputPath="%buildresults%\VS2012" > %buildresult%\VS2012_buildlog.txt
+%dotNetFrameworkPath% %GDWrapper2012% /property:Configuration=Release /target:Clean;Build /p:VisualStudioVersion=11.0 /p:OutputPath="%buildresults%\VS2012" > %buildresult%\VS2012_buildlog.txt
+%dotNetFrameworkPath% %VSPackage2012% /property:Configuration=Release /target:Clean;Build /p:VisualStudioVersion=11.0 /p:OutputPath="%buildresults%\VS2012" > %buildresult%\VS2012_buildlog.txt
+%dotNetFrameworkPath% %VSTasks2012% /property:Configuration=Release /target:Clean;Build /p:VisualStudioVersion=11.0 /p:OutputPath="%buildresults%\VS2012" > %buildresult%\VS2012_buildlog.txt
 echo Build Success
-
 
