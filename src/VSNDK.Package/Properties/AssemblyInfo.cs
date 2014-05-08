cb7c00a180fc81f9d9ba88ac81e5843c5807519a
Pawel Hofman <pawel@codetitans.pl>
2014-05-08 16:48:55 +0200

    VS-Package internals are visible by Test project

--- src/VSNDK.Package/Properties/AssemblyInfo.cs
+++ src/VSNDK.Package/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("VSNDK.Package")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("RIM")]
+[assembly: AssemblyCompany("BlackBerry")]
 [assembly: AssemblyProduct("VSNDK.Package")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -46,5 +46,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("VSNDK.Package_IntegrationTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f5a9bc4f77b74406a37bfd1c314ff87e73c3404311e95f1647205a3f7999c2454f3c816605cdccadcfe97dd444464057a267f838f84329235313cbaa186299f30d0db843613692df338a72829294dbba2bd2ed08e22863aeba0a5b69bb8a96cf558bb0230e590b2122555bddd35088248726a1ca953ca6f7bbfd6f8ce971aec7")]
-[assembly: InternalsVisibleTo("VSNDK.Package_UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f5a9bc4f77b74406a37bfd1c314ff87e73c3404311e95f1647205a3f7999c2454f3c816605cdccadcfe97dd444464057a267f838f84329235313cbaa186299f30d0db843613692df338a72829294dbba2bd2ed08e22863aeba0a5b69bb8a96cf558bb0230e590b2122555bddd35088248726a1ca953ca6f7bbfd6f8ce971aec7")]
+#if DEBUG
+[assembly: InternalsVisibleTo("VSNDK.Package.IntegrationTest")]
+[assembly: InternalsVisibleTo("VSNDK.Package.Test")]
+#endif
