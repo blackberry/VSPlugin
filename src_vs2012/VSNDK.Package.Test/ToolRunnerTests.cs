962580d48d3e66e935f59ebed3a61c30faf8a0ab
Pawel Hofman <pawel@codetitans.pl>
2014-05-09 16:33:24 +0200

    Added remove-application from the device feature

--- src_vs2012/VSNDK.Package.Test/ToolRunnerTests.cs
+++ src_vs2012/VSNDK.Package.Test/ToolRunnerTests.cs
@@ -8,10 +8,14 @@ namespace VSNDK.Package.Test
     [TestFixture]
     public class ToolRunnerTests
     {
+        const string IP = "10.0.0.127";
+        const string Password = "test";
+        readonly static string DebugTokenPath = RunnerDefaults.ConfigFileName("debugtoken.bar");
+
         [Test]
         public void LoadDebugTokenInfo()
         {
-            var runner = new DebugTokenInfoRunner(RunnerDefaults.TestToolsDirectory, RunnerDefaults.ConfigFileName("debugtoken.bar"));
+            var runner = new DebugTokenInfoRunner(RunnerDefaults.TestToolsDirectory, DebugTokenPath);
             var result = runner.Execute();
 
             Assert.IsTrue(result, "Unable to start the tool");
@@ -23,7 +27,7 @@ namespace VSNDK.Package.Test
         [Test]
         public void LoadDebugTokenAsync()
         {
-            var runner = new DebugTokenInfoRunner(RunnerDefaults.TestToolsDirectory, RunnerDefaults.ConfigFileName("debugtoken.bar"));
+            var runner = new DebugTokenInfoRunner(RunnerDefaults.TestToolsDirectory, DebugTokenPath);
 
             Assert.IsFalse(runner.IsProcessing);
             runner.ExecuteAsync();
@@ -49,7 +53,7 @@ namespace VSNDK.Package.Test
         [Ignore("Device-IP dependant test will only run somewhere correctly")]
         public void LoadDeviceInfo()
         {
-            var runner = new DeviceInfoRunner(RunnerDefaults.TestToolsDirectory, "10.0.0.127", "test");
+            var runner = new DeviceInfoRunner(RunnerDefaults.TestToolsDirectory, IP, Password);
             var result = runner.Execute();
 
             Assert.IsTrue(result, "Unable to start the tool");
@@ -61,13 +65,41 @@ namespace VSNDK.Package.Test
         [Ignore("Device-IP dependant test will only run somewhere correctly")]
         public void UploadDebugTokenInfo()
         {
-            var runner = new DebugTokenUploadRunner(RunnerDefaults.TestToolsDirectory, RunnerDefaults.ConfigFileName("debugtoken.bar"), "10.0.0.127", "test");
+            var runner = new DebugTokenUploadRunner(RunnerDefaults.TestToolsDirectory, DebugTokenPath, IP, Password);
             var result = runner.Execute();
 
             Assert.IsTrue(result, "Unable to start the tool");
             Assert.IsNotNull(runner.LastOutput);
             Assert.IsNull(runner.LastError);
             Assert.IsTrue(runner.UploadedSuccessfully);
+        }
+
+        [Test]
+        [Ignore("Device-IP dependant test will only run somewhere correctly")]
+        public void RemoveDebugTokenInfo()
+        {
+            // upload:
+            var uploader = new DebugTokenUploadRunner(RunnerDefaults.TestToolsDirectory, DebugTokenPath, IP, Password);
+            var result = uploader.Execute();
+
+            Assert.IsTrue(result, "Unable to start the tool");
+            Assert.IsTrue(uploader.UploadedSuccessfully);
+
+            // get info about the debug-token:
+            var informer = new DebugTokenInfoRunner(RunnerDefaults.TestToolsDirectory, DebugTokenPath);
+            result = informer.Execute();
+            Assert.IsTrue(result, "Unable to start the tool");
+            Assert.IsNotNull(informer.DebugToken);
+            Assert.IsNotNull(informer.DebugToken.ID);
+
+            // remove:
+            var cleaner = new ApplicationRemoveRunner(RunnerDefaults.TestToolsDirectory, informer.DebugToken.ID, IP, Password);
+            result = cleaner.Execute();
+
+            Assert.IsTrue(result, "Unable to start the tool");
+            Assert.IsNotNull(cleaner.LastOutput);
+            Assert.IsNull(cleaner.LastError);
+            Assert.IsTrue(cleaner.RemovedSuccessfully);
         }
     }
 }
