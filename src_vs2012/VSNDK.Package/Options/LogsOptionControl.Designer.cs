5d3d793163888d79a70dc8bda62a7f0957af05fd
Pawel Hofman <pawel@codetitans.pl>
2014-05-13 12:22:31 +0200

    Alignment fix on LogsOption control

--- src_vs2012/VSNDK.Package/Options/LogsOptionControl.Designer.cs
+++ src_vs2012/VSNDK.Package/Options/LogsOptionControl.Designer.cs
@@ -78,7 +78,7 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(530, 5);
+            this.button1.Location = new System.Drawing.Point(530, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
