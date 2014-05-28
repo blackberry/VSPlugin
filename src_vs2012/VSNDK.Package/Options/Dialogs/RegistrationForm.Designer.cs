9f64cef0d600cac17758ddafb891ea5a2844a8a1
Pawel Hofman <pawel@codetitans.pl>
2014-05-28 03:49:16 +0200

    Another round of UI design for registration form

--- src_vs2012/VSNDK.Package/Options/Dialogs/RegistrationForm.Designer.cs
+++ src_vs2012/VSNDK.Package/Options/Dialogs/RegistrationForm.Designer.cs
@@ -29,46 +29,53 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegistrationForm));
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.txtName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.bttOK = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.txtConfirmPassword = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.bttCreateToken = new System.Windows.Forms.Button();
             this.bttCreateCertificate = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.txtLog = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.cmbCertificate = new System.Windows.Forms.ComboBox();
-            this.txtCertName = new System.Windows.Forms.TextBox();
-            this.bttSelectCert = new System.Windows.Forms.Button();
             this.bttRefreshCert = new System.Windows.Forms.Button();
+            this.bttSelectCert = new System.Windows.Forms.Button();
+            this.txtCertName = new System.Windows.Forms.TextBox();
+            this.cmbCertificate = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
             this.bttCancel = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // label1
             // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(537, 121);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Info";
+            this.label1.Location = new System.Drawing.Point(12, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(519, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "To complete the registration both - BlackBerry ID token and developer certificate" +
+    " - are required.";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.Location = new System.Drawing.Point(12, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(519, 47);
+            this.label4.TabIndex = 0;
+            this.label4.Text = resources.GetString("label4.Text");
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(122, 50);
+            this.txtPassword.Location = new System.Drawing.Point(131, 129);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(113, 20);
@@ -77,7 +84,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 53);
+            this.label3.Location = new System.Drawing.Point(12, 132);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(56, 13);
             this.label3.TabIndex = 2;
@@ -85,7 +92,7 @@
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(122, 24);
+            this.txtName.Location = new System.Drawing.Point(131, 103);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(184, 20);
             this.txtName.TabIndex = 1;
@@ -93,7 +100,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 27);
+            this.label2.Location = new System.Drawing.Point(12, 106);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(70, 13);
             this.label2.TabIndex = 0;
@@ -102,7 +109,7 @@
             // bttOK
             // 
             this.bttOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.bttOK.Location = new System.Drawing.Point(393, 458);
+            this.bttOK.Location = new System.Drawing.Point(393, 496);
             this.bttOK.Name = "bttOK";
             this.bttOK.Size = new System.Drawing.Size(75, 23);
             this.bttOK.TabIndex = 3;
@@ -110,30 +117,9 @@
             this.bttOK.UseVisualStyleBackColor = true;
             this.bttOK.Click += new System.EventHandler(this.bttOK_Click);
             // 
-            // label4
-            // 
-            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label4.Location = new System.Drawing.Point(12, 25);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(519, 47);
-            this.label4.TabIndex = 0;
-            this.label4.Text = resources.GetString("label4.Text");
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.Location = new System.Drawing.Point(12, 82);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(519, 23);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "To complete the registration both - BlackBerry ID token and developer certificate" +
-    " - are required.";
-            // 
             // txtConfirmPassword
             // 
-            this.txtConfirmPassword.Location = new System.Drawing.Point(122, 76);
+            this.txtConfirmPassword.Location = new System.Drawing.Point(131, 155);
             this.txtConfirmPassword.Name = "txtConfirmPassword";
             this.txtConfirmPassword.PasswordChar = '*';
             this.txtConfirmPassword.Size = new System.Drawing.Size(113, 20);
@@ -142,7 +128,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 79);
+            this.label5.Location = new System.Drawing.Point(12, 158);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(93, 13);
             this.label5.TabIndex = 4;
@@ -150,18 +136,18 @@
             // 
             // bttCreateToken
             // 
-            this.bttCreateToken.Location = new System.Drawing.Point(418, 22);
+            this.bttCreateToken.Location = new System.Drawing.Point(15, 244);
             this.bttCreateToken.Name = "bttCreateToken";
-            this.bttCreateToken.Size = new System.Drawing.Size(113, 23);
+            this.bttCreateToken.Size = new System.Drawing.Size(110, 23);
             this.bttCreateToken.TabIndex = 11;
             this.bttCreateToken.Text = "Create &Token...";
             this.bttCreateToken.UseVisualStyleBackColor = true;
             // 
             // bttCreateCertificate
             // 
-            this.bttCreateCertificate.Location = new System.Drawing.Point(418, 53);
+            this.bttCreateCertificate.Location = new System.Drawing.Point(131, 244);
             this.bttCreateCertificate.Name = "bttCreateCertificate";
-            this.bttCreateCertificate.Size = new System.Drawing.Size(113, 23);
+            this.bttCreateCertificate.Size = new System.Drawing.Size(110, 23);
             this.bttCreateCertificate.TabIndex = 12;
             this.bttCreateCertificate.Text = "Create Ce&rtificate...";
             this.bttCreateCertificate.UseVisualStyleBackColor = true;
@@ -170,7 +156,9 @@
             // 
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Controls.Add(this.bttRefreshCert);
+            this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.bttSelectCert);
             this.groupBox2.Controls.Add(this.txtCertName);
             this.groupBox2.Controls.Add(this.cmbCertificate);
@@ -183,34 +171,37 @@
             this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Controls.Add(this.txtConfirmPassword);
             this.groupBox2.Controls.Add(this.txtPassword);
-            this.groupBox2.Location = new System.Drawing.Point(12, 139);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(537, 171);
+            this.groupBox2.Size = new System.Drawing.Size(537, 279);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Action";
+            this.groupBox2.Text = "BlackBerry 10 Devices";
             // 
-            // txtLog
+            // bttRefreshCert
             // 
-            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtLog.Location = new System.Drawing.Point(12, 316);
-            this.txtLog.Multiline = true;
-            this.txtLog.Name = "txtLog";
-            this.txtLog.ReadOnly = true;
-            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtLog.Size = new System.Drawing.Size(537, 136);
-            this.txtLog.TabIndex = 2;
+            this.bttRefreshCert.Location = new System.Drawing.Point(321, 208);
+            this.bttRefreshCert.Name = "bttRefreshCert";
+            this.bttRefreshCert.Size = new System.Drawing.Size(75, 23);
+            this.bttRefreshCert.TabIndex = 10;
+            this.bttRefreshCert.Text = "&Refresh";
+            this.bttRefreshCert.UseVisualStyleBackColor = true;
             // 
-            // label6
+            // bttSelectCert
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 108);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(57, 13);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "Certificate:";
+            this.bttSelectCert.Location = new System.Drawing.Point(321, 208);
+            this.bttSelectCert.Name = "bttSelectCert";
+            this.bttSelectCert.Size = new System.Drawing.Size(32, 23);
+            this.bttSelectCert.TabIndex = 9;
+            this.bttSelectCert.Text = "...";
+            this.bttSelectCert.UseVisualStyleBackColor = true;
+            // 
+            // txtCertName
+            // 
+            this.txtCertName.Location = new System.Drawing.Point(131, 208);
+            this.txtCertName.Name = "txtCertName";
+            this.txtCertName.Size = new System.Drawing.Size(184, 20);
+            this.txtCertName.TabIndex = 8;
             // 
             // cmbCertificate
             // 
@@ -220,48 +211,55 @@
             "Use current",
             "Create new",
             "Select existing"});
-            this.cmbCertificate.Location = new System.Drawing.Point(122, 102);
+            this.cmbCertificate.Location = new System.Drawing.Point(131, 181);
             this.cmbCertificate.MaxDropDownItems = 20;
             this.cmbCertificate.Name = "cmbCertificate";
             this.cmbCertificate.Size = new System.Drawing.Size(184, 21);
             this.cmbCertificate.TabIndex = 7;
             // 
-            // txtCertName
+            // label6
             // 
-            this.txtCertName.Location = new System.Drawing.Point(122, 129);
-            this.txtCertName.Name = "txtCertName";
-            this.txtCertName.Size = new System.Drawing.Size(184, 20);
-            this.txtCertName.TabIndex = 8;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 184);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Certificate:";
             // 
-            // bttSelectCert
+            // txtLog
             // 
-            this.bttSelectCert.Location = new System.Drawing.Point(312, 129);
-            this.bttSelectCert.Name = "bttSelectCert";
-            this.bttSelectCert.Size = new System.Drawing.Size(32, 23);
-            this.bttSelectCert.TabIndex = 9;
-            this.bttSelectCert.Text = "...";
-            this.bttSelectCert.UseVisualStyleBackColor = true;
-            // 
-            // bttRefreshCert
-            // 
-            this.bttRefreshCert.Location = new System.Drawing.Point(312, 127);
-            this.bttRefreshCert.Name = "bttRefreshCert";
-            this.bttRefreshCert.Size = new System.Drawing.Size(75, 23);
-            this.bttRefreshCert.TabIndex = 10;
-            this.bttRefreshCert.Text = "&Refresh";
-            this.bttRefreshCert.UseVisualStyleBackColor = true;
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(12, 390);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(537, 100);
+            this.txtLog.TabIndex = 2;
             // 
             // bttCancel
             // 
             this.bttCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.bttCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.bttCancel.Location = new System.Drawing.Point(474, 458);
+            this.bttCancel.Location = new System.Drawing.Point(474, 496);
             this.bttCancel.Name = "bttCancel";
             this.bttCancel.Size = new System.Drawing.Size(75, 23);
             this.bttCancel.TabIndex = 4;
             this.bttCancel.Text = "Ca&ncel";
             this.bttCancel.UseVisualStyleBackColor = true;
-            this.bttCancel.Click += new System.EventHandler(this.bttOK_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Location = new System.Drawing.Point(12, 297);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(537, 87);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "BlackBerry PlayBook";
             // 
             // RegistrationForm
             // 
@@ -269,12 +267,12 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.bttCancel;
-            this.ClientSize = new System.Drawing.Size(561, 493);
+            this.ClientSize = new System.Drawing.Size(561, 531);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.txtLog);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.bttCancel);
             this.Controls.Add(this.bttOK);
-            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -283,7 +281,6 @@
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Registration Form";
-            this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
@@ -293,7 +290,6 @@
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button bttOK;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label label3;
@@ -313,5 +309,6 @@
         private System.Windows.Forms.ComboBox cmbCertificate;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button bttCancel;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 }
