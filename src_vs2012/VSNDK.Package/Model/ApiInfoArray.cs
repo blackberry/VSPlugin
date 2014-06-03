ba1bd719166f04cebd48201f4a2231d56c4d8dae
Pawel Hofman <pawel@codetitans.pl>
2014-06-03 16:52:18 +0200

    Created model class to represent an API Level

---
+++ src_vs2012/VSNDK.Package/Model/ApiInfoArray.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RIM.VSNDK_Package.Model
+{
+    /// <summary>
+    /// Wrapper class for array of API info that belong to the same API Level.
+    /// </summary>
+    internal sealed class ApiInfoArray : ApiInfo, IEnumerable<ApiInfo>
+    {
+        public ApiInfoArray(string name, Version version, ApiInfo[] items)
+            : base(name, version)
+        {
+            Items = items ?? new ApiInfo[0];
+        }
+
+        #region Properties
+
+        public ApiInfo[] Items
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the number of stored items.
+        /// </summary>
+        public int Length
+        {
+            get { return Items.Length; }
+        }
+
+        #endregion
+
+        public IEnumerator<ApiInfo> GetEnumerator()
+        {
+            return (IEnumerator<ApiInfo>)Items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
