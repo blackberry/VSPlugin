9a5b79e5b253f17ca0e5949c2201a5d1cc0cbf80
Pawel Hofman <pawel@codetitans.pl>
2014-06-03 17:21:26 +0200

    Merged NDK and simulator download screens

--- src_vs2012/VSNDK.Package/Options/Dialogs/InstallForm.Designer.cs
+++ src_vs2012/VSNDK.Package/Options/Dialogs/InstallForm.Designer.cs
@@ -33,15 +33,20 @@
             this.panelInstalled = new System.Windows.Forms.FlowLayoutPanel();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.panelAvailable = new System.Windows.Forms.FlowLayoutPanel();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // bttOK
             // 
             this.bttOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.bttOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.bttOK.Location = new System.Drawing.Point(484, 418);
+            this.bttOK.Location = new System.Drawing.Point(497, 462);
             this.bttOK.Name = "bttOK";
             this.bttOK.Size = new System.Drawing.Size(75, 23);
             this.bttOK.TabIndex = 0;
@@ -53,9 +58,9 @@
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.panelInstalled);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(547, 197);
+            this.groupBox1.Size = new System.Drawing.Size(543, 197);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Installed";
@@ -69,19 +74,18 @@
             this.panelInstalled.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.panelInstalled.Location = new System.Drawing.Point(6, 19);
             this.panelInstalled.Name = "panelInstalled";
-            this.panelInstalled.Size = new System.Drawing.Size(535, 172);
+            this.panelInstalled.Size = new System.Drawing.Size(531, 172);
             this.panelInstalled.TabIndex = 0;
             this.panelInstalled.WrapContents = false;
             // 
             // groupBox2
             // 
-            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox2.Controls.Add(this.panelAvailable);
-            this.groupBox2.Location = new System.Drawing.Point(12, 215);
+            this.groupBox2.Location = new System.Drawing.Point(6, 210);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(547, 197);
+            this.groupBox2.Size = new System.Drawing.Size(543, 197);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Available";
@@ -95,9 +99,44 @@
             this.panelAvailable.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.panelAvailable.Location = new System.Drawing.Point(6, 19);
             this.panelAvailable.Name = "panelAvailable";
-            this.panelAvailable.Size = new System.Drawing.Size(535, 172);
+            this.panelAvailable.Size = new System.Drawing.Size(531, 172);
             this.panelAvailable.TabIndex = 0;
             this.panelAvailable.WrapContents = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(623, 418);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Simulator";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(558, 418);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "BlackBerry Native SDK";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(566, 444);
+            this.tabControl1.TabIndex = 3;
             // 
             // InstallNdkForm
             // 
@@ -105,9 +144,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.bttOK;
-            this.ClientSize = new System.Drawing.Size(571, 453);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(584, 497);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.bttOK);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
@@ -115,9 +153,11 @@
             this.Name = "InstallNdkForm";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "NDK Installation";
+            this.Text = "Installation";
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -129,5 +169,8 @@
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.FlowLayoutPanel panelInstalled;
         private System.Windows.Forms.FlowLayoutPanel panelAvailable;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabControl tabControl1;
     }
 }
