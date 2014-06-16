90ebca1483e0f22d0e20d51ee94108dbd46dcea7
Pawel Hofman <pawel@codetitans.pl>
2014-06-16 15:21:13 +0200

    Moved part of the old 'ViewModels' into Model
    The rest is highly bind with the UI, so it was moved back to the Package project.

---
+++ src_vs2012/NativeCore/Model/Size.cs
@@ -0,0 +1,35 @@
+﻿namespace BlackBerry.NativeCore.Model
+{
+    /// <summary>
+    /// Class describing 2-dimensional space.
+    /// </summary>
+    internal sealed class Size
+    {
+        public Size(uint width, uint height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        #region Properties
+
+        public uint Width
+        {
+            get;
+            private set;
+        }
+
+        public uint Height
+        {
+            get;
+            private set;
+        }
+        
+        #endregion
+
+        public override string ToString()
+        {
+            return string.Concat(Width, "x", Height);
+        }
+    }
+}
