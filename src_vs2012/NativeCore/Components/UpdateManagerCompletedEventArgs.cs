f3a972630a564b9dfb801df800a6ac19ba11139c
Pawel Hofman <pawel@codetitans.pl>
2014-06-16 16:02:52 +0200

    Moved UpdateManager to new location

---
+++ src_vs2012/NativeCore/Components/UpdateManagerCompletedEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BlackBerry.NativeCore.Model;
+
+namespace BlackBerry.NativeCore
+{
+    /// <summary>
+    /// Arguments passed along with Completed event of UpdateManager.
+    /// </summary>
+    public sealed class UpdateManagerCompletedEventArgs : EventArgs
+    {
+        internal UpdateManagerCompletedEventArgs(ApiLevelTarget target)
+        {
+            Target = target;
+        }
+
+        #region Properties
+
+        public ApiLevelTarget Target
+        {
+            get;
+            private set;
+        }
+
+        #endregion
+    }
+}
