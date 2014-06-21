1eba1a861f6af4b736e23e3640c0426417200e61
Pawel Hofman <pawel@codetitans.pl>
2014-06-21 02:01:45 +0200

    Proposed UI for 'missing NDK installed' dialog

--- src_vs2012/Package/Options/Dialogs/MissingNdkInstalledForm.Designer.cs
+++ src_vs2012/Package/Options/Dialogs/MissingNdkInstalledForm.Designer.cs
@@ -17,6 +17,16 @@
             {
                 components.Dispose();
             }
+
+            if (disposing)
+            {
+                if (_vm != null)
+                {
+                    _vm.Dispose();
+                    _vm = null;
+                }
+            }
+
             base.Dispose(disposing);
         }
 
@@ -29,18 +39,83 @@
         private void InitializeComponent()
         {
             this.bttClose = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.bttInstall = new System.Windows.Forms.Button();
+            this.bttStatus = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // bttClose
             // 
             this.bttClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.bttClose.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.bttClose.Location = new System.Drawing.Point(625, 268);
+            this.bttClose.Location = new System.Drawing.Point(341, 129);
             this.bttClose.Name = "bttClose";
             this.bttClose.Size = new System.Drawing.Size(75, 23);
             this.bttClose.TabIndex = 0;
             this.bttClose.Text = "&Close";
             this.bttClose.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.bttInstall);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(404, 111);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Selection";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(15, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(383, 47);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Toolset for building BlackBerry native application is missing. Please make sure t" +
+    "here is any NDK installed and select it from the list below.";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(15, 72);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(302, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // bttInstall
+            // 
+            this.bttInstall.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bttInstall.Location = new System.Drawing.Point(323, 70);
+            this.bttInstall.Name = "bttInstall";
+            this.bttInstall.Size = new System.Drawing.Size(75, 23);
+            this.bttInstall.TabIndex = 0;
+            this.bttInstall.Text = "Install...";
+            this.bttInstall.UseVisualStyleBackColor = true;
+            this.bttInstall.Click += new System.EventHandler(this.bttInstall_Click);
+            // 
+            // bttStatus
+            // 
+            this.bttStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bttStatus.Location = new System.Drawing.Point(12, 129);
+            this.bttStatus.Name = "bttStatus";
+            this.bttStatus.Size = new System.Drawing.Size(75, 23);
+            this.bttStatus.TabIndex = 2;
+            this.bttStatus.Text = "&Status...";
+            this.bttStatus.UseVisualStyleBackColor = true;
+            this.bttStatus.Click += new System.EventHandler(this.bttStatus_Click);
             // 
             // MissingNdkInstalledForm
             // 
@@ -48,7 +123,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.bttClose;
-            this.ClientSize = new System.Drawing.Size(712, 303);
+            this.ClientSize = new System.Drawing.Size(428, 164);
+            this.Controls.Add(this.bttStatus);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.bttClose);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
@@ -58,6 +135,7 @@
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Default BlackBerry NDK selection";
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -65,5 +143,10 @@
         #endregion
 
         private System.Windows.Forms.Button bttClose;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button bttInstall;
+        private System.Windows.Forms.Button bttStatus;
     }
 }
--- src_vs2012/Package/Options/Dialogs/MissingNdkInstalledForm.cs
+++ src_vs2012/Package/Options/Dialogs/MissingNdkInstalledForm.cs
@@ -1,12 +1,27 @@
 ﻿using System.Windows.Forms;
+using BlackBerry.Package.ViewModels;
 
 namespace BlackBerry.Package.Options.Dialogs
 {
     internal partial class MissingNdkInstalledForm : Form
     {
+        private ApiLevelOptionViewModel _vm = new ApiLevelOptionViewModel();
+
         public MissingNdkInstalledForm()
         {
             InitializeComponent();
+        }
+
+        private void bttStatus_Click(object sender, System.EventArgs e)
+        {
+            var form = new UpdateManagerStatusForm(_vm);
+            form.ShowDialog();
+        }
+
+        private void bttInstall_Click(object sender, System.EventArgs e)
+        {
+            var form = new InstallForm(_vm);
+            form.ShowDialog();
         }
     }
 }
