c5674fe625930a0a53e18755ea1b22ffd3c043ca
Pawel Hofman <pawel@codetitans.pl>
2014-06-25 17:39:02 +0200

    Started porting GDB parsert to C#

---
+++ src_vs2012/NativeCore/Debugger/Instruction.cs
@@ -0,0 +1,39 @@
+﻿namespace BlackBerry.NativeCore.Debugger
+{
+    /// <summary>
+    /// Class representing single GDB instruction.
+    /// </summary>
+    public sealed class Instruction
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets the ID of the instruction.
+        /// </summary>
+        public int ID
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the command text expected to be send from GDB.
+        /// </summary>
+        public string Command
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the response parsing instruction.
+        /// </summary>
+        public string Response
+        {
+            get;
+            private set;
+        }
+
+        #endregion
+    }
+}
