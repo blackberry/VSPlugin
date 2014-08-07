9fd6fab0bcc4953bbd185fba3aed7bebad1d5910
Pawel Hofman <pawel@codetitans.pl>
2014-08-07 15:38:00 +0200

    Added printing array in Java/C#/as string styles
    To help debugging cryptographic stuff.

--- src_vs2012/NativeCore/Diagnostics/QTraceLog.cs
+++ src_vs2012/NativeCore/Diagnostics/QTraceLog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Text;
 
 namespace BlackBerry.NativeCore.Diagnostics
 {
@@ -82,6 +83,56 @@ namespace BlackBerry.NativeCore.Diagnostics
                 string message = format == null ? null : string.Format(format, args);
                 Trace.WriteLine(TraceLog.FlatException(ex, message), Category);
             }
+        }
+
+        /// <summary>
+        /// Dumps an array into the logs.
+        /// </summary>
+        [Conditional("TRACE")]
+        [DebuggerStepThrough]
+        public static void PrintArray(string name, byte[] values)
+        {
+            StringBuilder result = new StringBuilder();
+
+            // print byte array for Java:
+            result.Append("byte[] ").Append(name).Append(" = new byte[ /* ").Append(values.Length).Append(" */ ] {");
+            int i = 0;
+            foreach (byte v in values)
+            {
+                if (i > 0)
+                    result.Append(", ");
+                i++;
+
+                // print signed value:
+                result.Append((sbyte)v);
+            }
+            result.Append("};");
+
+            // and the same array for C#:
+            result.AppendLine();
+            result.Append("// byte[] ").Append(name).Append("CS = new byte[ /* ").Append(values.Length).Append(" */ ] {");
+            i = 0;
+            foreach (byte v in values)
+            {
+                if (i > 0)
+                    result.Append(", ");
+                i++;
+
+                // print signed value:
+                result.Append(v);
+            }
+            result.Append("};");
+
+            // and also print it out as string:
+            result.AppendLine();
+            result.Append("String ").Append(name).Append("Str = \"00"); // fince in Java BigInteger is 2-complement, so we start with leading zeros, not to have a minus numbers...
+            foreach (var v in values)
+            {
+                result.Append(v.ToString("X2"));
+            }
+            result.Append("\";");
+
+            Trace.WriteLine(result.ToString());
         }
     }
 }
