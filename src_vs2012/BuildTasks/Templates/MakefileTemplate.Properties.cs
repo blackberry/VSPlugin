603bcaff04b3cfdfcda7ca361fdaf25bc2d58327
Pawel Hofman <pawel@codetitans.pl>
2014-10-06 22:27:44 +0200

    Started porting makefile-generator to T4

---
+++ src_vs2012/BuildTasks/Templates/MakefileTemplate.Properties.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace BlackBerry.BuildTasks.Templates
+{
+    partial class MakefileTemplate
+    {
+        #region Properties
+
+        public string SolutionName
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets the current version of the current library.
+        /// </summary>
+        public Version Version
+        {
+            get { return Assembly.GetExecutingAssembly().GetName().Version; }
+        }
+
+        #endregion
+    }
+}
