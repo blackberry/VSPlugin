2bf4721d566db581d2589dedee538cda656e7528
Pawel Hofman <pawel@codetitans.pl>
2014-10-21 02:35:32 +0200

    PuppetMaster adds item-templates in fancy way
    It is enough to specify any number of the template files via 'FILE' custom-property of
    .vsz file. Also the path supports two markers '->' and '#>' to override
    whole name and extension only of developer supplied item name.
    
    Sample of .vsz file:
    
    VSWizard 7.0
    Wizard={0cb934ba-06c8-4919-9498-bc6517940bcd}
    Param="FILE = any/file/path.xml -> 'this name instead' "
    Param="FILE = any/file/path.xml #> .txt "

--- src_vs2012/Package/Wizards/BaseWizardEngine.cs
+++ src_vs2012/Package/Wizards/BaseWizardEngine.cs
@@ -1,15 +1,208 @@
-﻿using EnvDTE;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using BlackBerry.NativeCore.Diagnostics;
+using BlackBerry.Package.Model.Wizards;
+using EnvDTE;
+using EnvDTE80;
 
 namespace BlackBerry.Package.Wizards
 {
     /// <summary>
     /// Base class for any wizard engines responsible for collecting all information from developer to correctly create new project or project item.
     /// </summary>
+    [ComVisible(true)]
     public abstract class BaseWizardEngine : IDTWizard
     {
-        public void Execute(object application, int hwndOwner, ref object[] contextParams, ref object[] customParams, ref wizardResult returnValue)
+        /// <summary>
+        /// Entry point of the wizard engine.
+        /// </summary>
+        void IDTWizard.Execute(object application, int hwndOwner, ref object[] contextParams, ref object[] customParams, ref wizardResult returnValue)
         {
-            
+            var dte = application as DTE2;
+            if (dte == null)
+            {
+                TraceLog.WarnLine("Invalid instance of DTE received ({0})", GetType().Name);
+                returnValue = wizardResult.wizardResultFailure;
+                return;
+            }
+
+            // parameters passed to the wizard are described here:
+            // http://msdn.microsoft.com/en-us/library/bb164728.aspx && http://msdn.microsoft.com/en-us/library/tz690efs.aspx
+
+            var wizardType = contextParams[0] != null ? contextParams[0].ToString() : null;
+
+            // add new project:
+            if (String.Compare(wizardType, NewProjectParams.TypeGuid, StringComparison.OrdinalIgnoreCase) == 0)
+            {
+                returnValue = ExecuteNewProject(dte, new IntPtr(hwndOwner), new NewProjectParams(contextParams), ParseCustomParams(customParams));
+                return;
+            }
+
+            // add new project item:
+            if (String.Compare(wizardType, NewProjectItemParams.TypeGuid, StringComparison.OrdinalIgnoreCase) == 0)
+            {
+                returnValue = ExecuteNewProjectItem(dte, new IntPtr(hwndOwner), new NewProjectItemParams(contextParams), ParseCustomParams(customParams));
+                return;
+            }
+
+            TraceLog.WarnLine("Unknown wizard type to launch ({0}: {1})", GetType().Name, wizardType);
+            returnValue = wizardResult.wizardResultFailure;
         }
+
+        private KeyValuePair<string, string>[] ParseCustomParams(object[] customParams)
+        {
+            var result = new List<KeyValuePair<string,string>>();
+
+            if (customParams != null)
+            {
+                foreach (var cp in customParams)
+                {
+                    if (cp != null)
+                    {
+                        var textParam = cp.ToString();
+
+                        if (!String.IsNullOrEmpty(textParam))
+                        {
+                            int separatorIndex = textParam.IndexOf('=');
+
+                            if (separatorIndex < 0)
+                            {
+                                result.Add(new KeyValuePair<string, string>(textParam.Trim(), String.Empty));
+                            }
+                            else
+                            {
+                                result.Add(new KeyValuePair<string, string>(textParam.Substring(0, separatorIndex).Trim(), textParam.Substring(separatorIndex + 1).Trim()));
+                            }
+                        }
+                    }
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        /// <summary>
+        /// Gets the C/C++ code safe name from specified 'name' (understood as file name).
+        /// </summary>
+        protected static string CreateSafeName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return "New1";
+
+            var result = new StringBuilder();
+
+            // remove all non-standard letters from name:
+            for (int i = 0; i < name.Length; i++)
+            {
+                var c = name[i];
+                if (char.IsLetterOrDigit(c))
+                {
+                    result.Append(c);
+                }
+                else
+                {
+                    if (c == '_' || c == ' ')
+                    {
+                        result.Append('_');
+                    }
+                }
+
+            }
+
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// Get the 'source' part of the item specified path.
+        /// Format of the path:
+        ///  * &lt;source-template&gt;
+        ///  * &lt;source-template&gt; -&gt; &lt;new-name&gt;
+        ///  * &lt;source-template&gt; #&gt; &lt;new-extension&gt;
+        /// </summary>
+        protected string GetSourceName(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return null;
+
+            var index = path.IndexOf("->", 0, StringComparison.Ordinal);
+            if (index >= 0)
+            {
+                return Unwrap(path.Substring(0, index).Trim());
+            }
+
+            index = path.IndexOf("#>", 0, StringComparison.Ordinal);
+            if (index >= 0)
+            {
+                return Unwrap(path.Substring(0, index).Trim());
+            }
+
+            return Unwrap(path);
+        }
+
+        /// <summary>
+        /// Gets the 'destination' part of the item specified path and applies the transformation.
+        /// </summary>
+        protected string GetDestinationName(string itemName, string path)
+        {
+            if (string.IsNullOrEmpty(itemName))
+                throw new ArgumentNullException("itemName");
+            if (string.IsNullOrEmpty(path))
+                throw new ArgumentNullException("path");
+
+            var index = path.IndexOf("->", 0, StringComparison.Ordinal);
+            if (index >= 0)
+            {
+                var destinationRequest = Unwrap(path.Substring(index + 2).Trim());
+
+                if (!string.IsNullOrEmpty(destinationRequest))
+                {
+                    // replace item's name:
+                    return Path.Combine(Path.GetDirectoryName(itemName), destinationRequest);
+                }
+            }
+
+            index = path.IndexOf("#>", 0, StringComparison.Ordinal);
+            if (index >= 0)
+            {
+                var destinationRequest = Unwrap(path.Substring(index + 2).Trim());
+
+                if (!string.IsNullOrEmpty(destinationRequest))
+                {
+                    // change item's extension:
+                    return Path.ChangeExtension(itemName, destinationRequest);
+                }
+            }
+
+            return itemName;
+        }
+
+        private static string Unwrap(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return null;
+
+            if ((path[0] == '"' && path[path.Length - 1] == '"')
+                || (path[0] == '\'' && path[path.Length - 1] == '\''))
+                return path.Substring(1, path.Length - 2);
+
+            return path;
+        }
+
+        #region Abstract Methods
+
+        /// <summary>
+        /// Method that creates new project for existing or new solution.
+        /// </summary>
+        internal abstract wizardResult ExecuteNewProject(DTE2 dte, IntPtr owner, NewProjectParams context, KeyValuePair<string, string>[] customParams);
+
+        /// <summary>
+        /// Method that creates new project item for existing project.
+        /// </summary>
+        internal abstract wizardResult ExecuteNewProjectItem(DTE2 dte, IntPtr owner, NewProjectItemParams context, KeyValuePair<string, string>[] customParams);
+
+        #endregion
     }
 }
--- src_vs2012/Package/Wizards/PuppetMasterWizardEngine.cs
+++ src_vs2012/Package/Wizards/PuppetMasterWizardEngine.cs
@@ -1,50 +1,120 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using BlackBerry.NativeCore.Components;
+using BlackBerry.NativeCore.Diagnostics;
+using BlackBerry.Package.Model.Wizards;
+using BlackBerry.Package.ViewModels;
 using EnvDTE;
 using EnvDTE80;
 
 namespace BlackBerry.Package.Wizards
 {
+    /// <summary>
+    /// Advanced template wizard, that will do a real magic based on parameters specified from .vsz files of BlackBerry new items and new projects.
+    /// </summary>
     [ComVisible(true)]
     [Guid("0cb934ba-06c8-4919-9498-bc6517940bcd")]
     [ProgId("BlackBerry.PuppetMasterWizardEngine")]
-    public sealed class PuppetMasterWizardEngine : IDTWizard
+    public sealed class PuppetMasterWizardEngine : BaseWizardEngine
     {
-        public PuppetMasterWizardEngine()
+        private static string _wizardDataFolder;
+
+        /// <summary>
+        /// Gets the path, where the additional data files of the wizard are located.
+        /// </summary>
+        public static string WizardDataFolder
         {
-            string path = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath), "Templates"));
-            WizardDataFolder = Path.Combine(path, @"VCWizards\BlackBerry");
-        }
-
-        private string WizardDataFolder
-        {
-            get;
-            set;
-        }
-
-        void IDTWizard.Execute(object application, int hwndOwner, ref object[] contextParams, ref object[] customParams, ref wizardResult returnValue)
-        {
-            // parameters passed to the wizard are described here:
-            // http://msdn.microsoft.com/en-us/library/bb164728.aspx && http://msdn.microsoft.com/en-us/library/tz690efs.aspx
-
-            returnValue = wizardResult.wizardResultSuccess;
-
-            var dte = application as DTE2;
-            var wizardType = contextParams[0].ToString();
-
-            // add new project:
-            if (string.Compare(wizardType, "{0f90e1d0-4999-11d1-b6d1-00a0c90f2744}", StringComparison.OrdinalIgnoreCase) == 0)
+            get
             {
-                if (dte != null)
+                if (string.IsNullOrEmpty(_wizardDataFolder))
                 {
-                    dte.Solution.AddFromFile(Path.Combine(WizardDataFolder, "opengl-default.vcxproj"));
+                    string path = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath), "Templates"));
+                    _wizardDataFolder = Path.Combine(path, @"VCWizards\BlackBerry");
+                }
+
+                return _wizardDataFolder;
+            }
+        }
+
+        /// <summary>
+        /// Method that creates new project for existing or new solution.
+        /// </summary>
+        internal override wizardResult ExecuteNewProject(DTE2 dte, IntPtr owner, NewProjectParams context, KeyValuePair<string, string>[] customParams)
+        {
+            dte.Solution.AddFromFile(Path.Combine(WizardDataFolder, "opengl-default.vcxproj"));
+
+            MessageBox.Show("I am a custom wizard engine!");
+            return wizardResult.wizardResultSuccess;
+        }
+
+        /// <summary>
+        /// Method that creates new project item for existing project.
+        /// </summary>
+        internal override wizardResult ExecuteNewProjectItem(DTE2 dte, IntPtr owner, NewProjectItemParams context, KeyValuePair<string, string>[] customParams)
+        {
+            var project = context.ProjectItems.ContainingProject;
+            var tokenProcessor = CreateTokenProcessor(context, context.ItemName);
+
+            foreach (var data in customParams)
+            {
+                if (string.Compare(data.Key, "file", StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    var templatePath = Path.Combine(WizardDataFolder, GetSourceName(data.Value));
+                    var destinationPath = GetDestinationName(context.ItemName, data.Value);
+
+                    // update tokens withing the file and copy to destination:
+                    tokenProcessor.UntokenFile(templatePath, destinationPath);
+
+                    // then add that file to the project items:
+                    project.ProjectItems.AddFromFile(destinationPath);
+                    TraceLog.WriteLine("Added file: \"{0}\"", destinationPath);
                 }
             }
 
-            MessageBox.Show("I am a custom wizard engine!");
+            return wizardResult.wizardResultSuccess;
+        }
+
+        private TokenProcessor CreateTokenProcessor(ContextParams context, string destinationName)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+            if (string.IsNullOrEmpty(destinationName))
+                throw new ArgumentNullException("destinationName");
+
+            var tokenProcessor = new TokenProcessor();
+            var name = Path.GetFileNameWithoutExtension(destinationName);
+            var ext = Path.GetExtension(destinationName);
+            var safeName = CreateSafeName(name);
+
+            var author = PackageViewModel.Instance.Developer.Name ?? "Example Inc.";
+            var authorID = "ABCD1234";
+            var now = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+
+            tokenProcessor.AddReplace(@"[!output PROJECT_NAME]", context.ProjectName);
+            tokenProcessor.AddReplace("$projectname$", context.ProjectName);
+            tokenProcessor.AddReplace("$ProjectName$", context.ProjectName);
+            tokenProcessor.AddReplace("$safename$", safeName);
+            tokenProcessor.AddReplace("$SafeName$", safeName);
+            tokenProcessor.AddReplace("$filename$", name);
+            tokenProcessor.AddReplace("$FileName$", name);
+            tokenProcessor.AddReplace("$ext$", ext);
+            tokenProcessor.AddReplace("$Ext$", ext);
+            tokenProcessor.AddReplace("$extension$", ext);
+            tokenProcessor.AddReplace("$Extension$", ext);
+            tokenProcessor.AddReplace("$user$", Environment.UserName);
+            tokenProcessor.AddReplace("$User$", Environment.UserName);
+            tokenProcessor.AddReplace("$author$", author);
+            tokenProcessor.AddReplace("$Author$", author);
+            tokenProcessor.AddReplace("$authorid$", authorID);
+            tokenProcessor.AddReplace("$AuthorID$", authorID);
+            tokenProcessor.AddReplace("$now$", now);
+            tokenProcessor.AddReplace("$Now$", now);
+
+            return tokenProcessor;
         }
     }
 }
